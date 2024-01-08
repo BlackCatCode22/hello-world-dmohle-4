58b82eb353e72a1382edbd439f9caf609a731da8
dmohle <10038913+dmohle@users.noreply.github.com>
2024-01-08 00:06:03 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to my short programs" << endl;
+
+    int num1 = 2;
+    int num2 = 4;
+    int sum = 0;
+
+    sum = num1 + num2;
+
+    cout << "\n The sum of " << num1 << " and " << num2 << " is " << sum << "\n\n";
+
+    return 0;
+}
